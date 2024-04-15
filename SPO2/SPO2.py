3c8b2e56deb14aeb442110304ad9db59ccc31a36
mbarki55 <aitmbarek2oumaima@gmail.com>
2024-04-15 16:01:09 +0100

    spo2 final

--- SPO2/SPO2.py
+++ SPO2/SPO2.py
@@ -1,5 +1,16 @@
-with open("SPO2_data.log", "r") as file:
-  
+# r'C:\Users\MEDIOT\ABA_project\ABA_project\SPO2\SPO2_data.log', "r"
+
+with open(r'C:\Users\MEDIOT\ABA_project\ABA_project\SPO2\SPO2_data.log', "r+") as file:
+   
     data = file.read()
 
-print(data)
+    explodedData=data.split("55AA0604")
+    explodedData = [element[:-4] for element in explodedData[1:-1]]
+    print(explodedData)
+    decimal_values = [int(element, 16) for element in explodedData]
+    decimal_values=filter (lambda x:x<=100, decimal_values)
+    print(list(decimal_values))
+
+
+
+
