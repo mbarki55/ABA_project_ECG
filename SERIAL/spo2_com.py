0d9159e717f5008709536b0b2a3277db34d5f28f
mbarki55 <aitmbarek2oumaima@gmail.com>
2024-05-02 15:20:08 +0100

    initial code

--- SERIAL/spo2_com.py
+++ SERIAL/spo2_com.py
@@ -1,33 +1,49 @@
-# import serial
-
-# port = 'COM3'  
-# baud_rate = 115200  
-
-# ser = serial.Serial(port, baud_rate, timeout=1)
-
-# def read_write_data():
-#     ser.write(b'\x55\xAA\x04\x03\x01\xF7')
-#     try:
-#         response = ser.read()
-#         print("Received response in hexadecimal:", response.hex())
-#         if b"\x55\xaa" in response:
-#             print("Valid response received.")
-#         else:
-#             print("Invalid response format:", response.hex())
-#     except serial.SerialTimeoutException:
-#         print("Timeout occurred while waiting for response.")
-
-# read_write_data()
-
 import serial
 
-def send_data_and_receive_response():
-    port_name = "COM3"
-    baud_rate = 115200
-    data_to_send = b'\x55\xAA\x04\x03\x01\xF7'
+# Define the serial port and baud rate
+serial_port = 'COM3'
+baud_rate = 115200
 
-    with serial.Serial(port_name, baud_rate, timeout=1) as ser:
+# Data to send
+data_to_send = b'\x55\xAA\x04\xFE\x00\xFC'
+# data_to_send = b'\x55\xAA\x04\x02\x01\xF8'
+ser = serial.Serial(serial_port, baud_rate, timeout=1)
+try:
+    # Connect to the serial port
+    if ser.isOpen():
+        print(f"Connected to {serial_port} at {baud_rate} baud.")
+        
+        # Send data once
         ser.write(data_to_send)
-        return ser.readline().hex()
+        print("Sent data:", data_to_send)
+        
+        # Enter loop for receiving data
+        while True:
+            data_table=[]
+            # Read response from the serial port
+            response = ser.read()
+            print(response)
+            if(response == b"\xfe"):
+                print("55 found")
+                data_table.append(response)
+
+                while True :
+                    response= ser.read()
+                    if (response !=b"\xfe"):
+                        data_table.append(response.hex())
+                    else:
+                        print(data_table)
+                        if data_table[3] == b"\x55":
+                                print("SPO2 found")
+
+                        break
+        
     
-send_data_and_receive_response()
+except serial.SerialException as e:
+    print(f"Error opening serial port: {e}")
+except KeyboardInterrupt:
+    print("Keyboard interrupt. Exiting...")
+finally:
+    if ser.isOpen():
+        ser.close()
+        print("Serial port closed.")
