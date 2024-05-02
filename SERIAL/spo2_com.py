b95fafb6d15ee8b43799cc30dc8ce754e22c43a5
mbarki55 <aitmbarek2oumaima@gmail.com>
2024-05-02 18:55:55 +0100

    com final besoin dune optimisation

--- SERIAL/spo2_com.py
+++ SERIAL/spo2_com.py
@@ -1,8 +1,18 @@
 import serial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import numpy as np
+import time
 
 # Define the serial port and baud rate
 serial_port = 'COM3'
 baud_rate = 115200
+x_values = []
+y_decimal_value = []
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 
 # Data to send
 data_to_send = b'\x55\xAA\x04\xFE\x00\xFC'
@@ -19,26 +29,43 @@ try:
         
         # Enter loop for receiving data
         while True:
-            data_table=[]
+            data_table = []
             # Read response from the serial port
             response = ser.read()
-            print(response)
-            if(response == b"\xfe"):
-                print("55 found")
+            # print(response)
+            if response == b"\x55":
+                # print("55 found")
                 data_table.append(response)
 
-                while True :
-                    response= ser.read()
-                    if (response !=b"\xfe"):
-                        data_table.append(response.hex())
+                while True:
+                    response = ser.read()
+                    if response != b"\x55":
+                        data_table.append(response)
                     else:
-                        print(data_table)
-                        if data_table[3] == b"\x55":
-                                print("SPO2 found")
-
+                        # print(data_table)
+                        # print(len(data_table))
                         break
-        
-    
+                if (len(data_table) >= 6):
+                    if data_table[3] == b"\xFE" :
+                        hex_value = data_table[4].hex()  
+                        decimal_value = int(hex_value, 16) 
+                        print("SPO2 found",decimal_value)
+                        
+                        x_values.append(time.time())
+                        y_decimal_value.append(decimal_value)
+                                
+                    ax.clear()            
+                    ax.plot(x_values, y_decimal_value)
+                    
+                    plt.xlabel('X')
+                    plt.ylabel('Y')
+                    plt.title('Received Response Plot')
+                    ax = plt.gca()
+                    #plt.clf()
+                    plt.pause(0.01)
+                    #break
+                    
+
 except serial.SerialException as e:
     print(f"Error opening serial port: {e}")
 except KeyboardInterrupt:
